ebd9c83148ae1ce559601c27d2e9bb1078f6935c
qianqian <qzhao0544@gmail.com>
2025-10-07 19:38:06 +1100

    transaction page update

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0d38e3b2974a4f72e7820cc78d586cc6845a42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afbad37facc2f28e9a9c52b56f3c42f32ebccf21")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
