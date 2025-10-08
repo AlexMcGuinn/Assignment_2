77dfdd6aeafd1f15ff3a672fca95f0d0d12ecb84
qianqian <qzhao0544@gmail.com>
2025-10-08 13:26:10 +1100

    budget page updated

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afbad37facc2f28e9a9c52b56f3c42f32ebccf21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebd9c83148ae1ce559601c27d2e9bb1078f6935c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
